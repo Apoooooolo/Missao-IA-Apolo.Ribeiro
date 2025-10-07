0e8800fdf49f79e78e8bd9cae4ef8d29a0584c90
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-10-07 12:01:45 -0300

    gd

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const caixapPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 
 
 
