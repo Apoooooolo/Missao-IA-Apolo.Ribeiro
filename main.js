acf9081ba2119f294e06ea1ba8a01947e18a26a7
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-10-21 11:59:31 -0300

    fxggf

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Se fosse possível ...;
+    caixaPerguntas.textContent = "Se fosse possível ...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = " ";
 }
