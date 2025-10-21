788d65829d0d3d763abd37b5184fb9c13e60d383
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-10-21 11:52:22 -0300

    htsd

--- main.js
+++ main.js
@@ -92,8 +92,15 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
 function mostraPerguntas(){
+
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
